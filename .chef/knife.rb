04a8070ae8a7b747a18a28196a512512eba7c582
Kennie Fox <kfox@digitalocean.com>
2015-08-21 20:55:27 -0400

    Adding the Chef starter kit.

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "kfox"
+client_key               "#{current_dir}/kfox.pem"
+validation_client_name   "skynet-validator"
+validation_key           "#{current_dir}/skynet-validator.pem"
+chef_server_url          "https://chefserver.chefgoruby.com/organizations/skynet"
+cookbook_path            ["#{current_dir}/../cookbooks"]
